190018b528b21c320e99d87a45b6dcd6e1d6fea6
Saikat Mondal <saikatm1997@gmail.com>
2021-09-12 18:18:57 +0530

    Update Firebase.js
    remove credentials

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,14 +4,14 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhMUWKEaU1IUb7_YL9cAYqc1zTH5LMxBA",
-  authDomain: "alliswell-bf686.firebaseapp.com",
-  projectId: "alliswell-bf686",
-  databaseURL: "https://alliswell-bf686-default-rtdb.firebaseio.com",
-  storageBucket: "alliswell-bf686.appspot.com",
-  messagingSenderId: "87465855106",
-  appId: "1:87465855106:web:176b62c113e41eeaee0c9b",
-  measurementId: "G-Z14XS67PLV",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
